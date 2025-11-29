5d7706d3ae6368d8c953450818e5cb31a0f3520f
hughesjonathan181-creator <hughesjonathan181@gmail.com>
2025-11-29 13:08:53 -0500

    Add --ssl-no-revoke option to curl command

--- .ci/get_keys-windows.sh
+++ .ci/get_keys-windows.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-curl -fLo "./llvm.lock" "https://github.com/RPCS3/llvm-mirror/releases/download/custom-build-win-${LLVM_VER}/llvmlibs_mt.7z.sha256"
+curl --ssl-no-revoke -fLo "./llvm.lock" "https://github.com/RPCS3/llvm-mirror/releases/download/custom-build-win-${LLVM_VER}/llvmlibs_mt.7z.sha256"
